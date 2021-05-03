d87cc6c3db2fdd5a6aac091aca12acedc6c58e03
Bhupendra Singh <bhupendrasingh.ec18@gmail.com>
2021-05-04 01:02:21 +0530

    pagination is running

--- App/src/app/components/adventure/adventure.component.ts
+++ App/src/app/components/adventure/adventure.component.ts
@@ -8,11 +8,23 @@ import { DataService } from 'src/app/services/data.service';
 })
 export class AdventureComponent implements OnInit {
   result: any;
+  data: any;
+  defaultPageSize: any = 0;
+  active: any = false;
+  prevPage: any = 0;
+  nextpage: any = 2;
+  mainPage: any = 1;
 
   constructor(private service: DataService) { }
   ngOnInit(): void {
     this.service.getMethod('http://skunkworks.ignitesol.com:8000/books/?topic=adventure').subscribe((data: any) => {
-      this.result = data.results
+      this.data = data;
+      this.result = data.results;
+      let pageSize = 1;
+      this.defaultPageSize = Number(pageSize);
+      this.prevPage = this.defaultPageSize;
+      this.mainPage += this.defaultPageSize;
+      this.nextpage += this.defaultPageSize;
     })
   }
 
@@ -25,5 +37,55 @@ export class AdventureComponent implements OnInit {
     this.service.getSearchMethod(value, '/?topic=adventure').subscribe((data: any) => {
       this.result = data.results
     })
+  }
+
+  // pagination for previous page
+  PrevPagination() {
+    if (this.prevPage === 1) {
+      this.prevPage = 1;
+    } else {
+      this.prevPage = this.prevPage - 1;
+    }
+    this.mainPage = this.prevPage + 1;
+    this.nextpage = this.mainPage + 1
+    this.service.getMethod(`http://skunkworks.ignitesol.com:8000/books/?page=${this.prevPage}&topic=adventure`).subscribe((data: any) => {
+      this.result = data.results
+    })
+    if (this.prevPage === 1) {
+      this.prevPage = 1;
+    } else {
+      this.prevPage = this.mainPage - 1;
+    }
+    if (this.mainPage === 2) {
+      this.mainPage = 2;
+    } else {
+      this.mainPage = this.nextpage - 1;
+    }
+    if (this.nextpage === 3) {
+      this.nextpage = 3;
+    } else {
+      this.nextpage = this.nextpage;
+    }
+  }
+
+  // pagination for static (mid)
+  pagination() {
+    this.service.getMethod(`http://skunkworks.ignitesol.com:8000/books/?page=${this.mainPage}&topic=fiction`).subscribe((data: any) => {
+      this.result = data.results
+    })
+  }
+
+  // pagination for nextpage
+  nextPagination() {
+    this.prevPage = this.mainPage - this.prevPage;
+    this.mainPage = this.mainPage;
+    this.nextpage = this.mainPage + 1
+    this.service.getMethod(`http://skunkworks.ignitesol.com:8000/books/?page=${this.nextpage}&topic=fiction`).subscribe((data: any) => {
+      console.log(data);
+      this.result = data.results
+    });
+    this.prevPage = this.mainPage;
+    this.mainPage = this.nextpage;
+    this.nextpage = this.nextpage + 1
   }
 }
--- App/src/app/components/fiction/fiction.component.ts
+++ App/src/app/components/fiction/fiction.component.ts
@@ -8,12 +8,24 @@ import { DataService } from 'src/app/services/data.service';
 })
 export class FictionComponent implements OnInit {
   result: any;
+  data: any;
+  defaultPageSize: any = 0;
+  active: any = false;
+  prevPage: any = 0;
+  nextpage: any = 2;
+  mainPage: any = 1;
 
   constructor(private service: DataService) { }
 
   ngOnInit(): void {
     this.service.getMethod('http://skunkworks.ignitesol.com:8000/books/?topic=fiction').subscribe((data: any) => {
-      this.result = data.results
+      this.data = data;
+      this.result = data.results;
+      let pageSize = 1;
+      this.defaultPageSize = Number(pageSize);
+      this.prevPage = this.defaultPageSize;
+      this.mainPage += this.defaultPageSize;
+      this.nextpage += this.defaultPageSize;
     })
   }
 
@@ -24,8 +36,63 @@ export class FictionComponent implements OnInit {
       value = search.target.value.replace(/\s/g, "%20")
     }
     this.service.getSearchMethod(value, '/?topic=fiction').subscribe((data: any) => {
+      this.data = data;
+      this.result = data.results;
+      let pageSize = 1;
+      this.defaultPageSize = Number(pageSize);
+      this.prevPage = this.defaultPageSize;
+      this.mainPage += this.defaultPageSize;
+      this.nextpage += this.defaultPageSize;
+    })
+  }
+
+  // pagination for previous page
+  PrevPagination() {
+    if (this.prevPage === 1) {
+      this.prevPage = 1;
+    } else {
+      this.prevPage = this.prevPage - 1;
+    }
+    this.mainPage = this.prevPage + 1;
+    this.nextpage = this.mainPage + 1;
+    this.service.getMethod(`http://skunkworks.ignitesol.com:8000/books/?page=${this.prevPage}&topic=fiction`).subscribe((data: any) => {
+      this.result = data.results
+    })
+    if (this.prevPage === 1) {
+      this.prevPage = 1;
+    } else {
+      this.prevPage = this.mainPage - 1;
+    }
+    if (this.mainPage === 2) {
+      this.mainPage = 2;
+    } else {
+      this.mainPage = this.nextpage - 1;
+    }
+    if (this.nextpage === 3) {
+      this.nextpage = 3;
+    } else {
+      this.nextpage = this.nextpage;
+    }
+  }
+
+  // pagination for static (mid)
+  pagination() {
+    this.service.getMethod(`http://skunkworks.ignitesol.com:8000/books/?page=${this.mainPage}&topic=fiction`).subscribe((data: any) => {
       this.result = data.results
     })
   }
 
+  // pagination for nextpage
+  nextPagination() {
+    this.prevPage = this.mainPage - this.prevPage;
+    this.mainPage = this.mainPage;
+    this.nextpage = this.mainPage + 1
+    this.service.getMethod(`http://skunkworks.ignitesol.com:8000/books/?page=${this.nextpage}&topic=fiction`).subscribe((data: any) => {
+      console.log(data);
+      this.result = data.results
+    });
+    this.prevPage = this.mainPage;
+    this.mainPage = this.nextpage;
+    this.nextpage = this.nextpage + 1
+  }
 }
